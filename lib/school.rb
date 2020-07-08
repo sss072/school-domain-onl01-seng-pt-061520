94ce450b2c963468f61a8667369ed4a4118a0674
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-07-08 23:38:24 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,14 +1,10 @@
 class School 
+  attr_accessor
   def initialize(name)
     @name = name
     @roster = {}
   end
-  def name=(name)
-    @name = name 
-  end
-  def name 
-    @name 
-  end
+  
   def add_student(student_name, grade)
     @roster["grade"] = []
     @roster["grade"] << "student_name"
