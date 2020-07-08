8c3fbb9efbedd233b83cc5f505b3cffe2fd53f01
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-07-08 23:29:39 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,13 @@
-# code here!
+class School 
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  def name=(name)
+    @name = name 
+  end
+  def name 
+    @name 
+  end
+  def add_student(student_name, grade)
+    @roster["grade"] = 
