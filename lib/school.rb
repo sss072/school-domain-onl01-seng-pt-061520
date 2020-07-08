434380d2e3a6e562195fab10e7c8700248b1bfb4
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-07-08 23:49:05 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,8 +6,8 @@ class School
   end
   
   def add_student(student_name, grade)
-    @roster["grade"] ||= []
-    @roster["grade"] << "student_name"
+    @roster[grade] ||= []
+    @roster[grade] << "student_name"
   end
   def grade(grade_given)
     @roster[grade_given]
